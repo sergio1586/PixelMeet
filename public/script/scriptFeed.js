b3e6c4dfb29710f683b024b3ffb5c2f87cd238eb
Your Name <you@example.com>
2024-05-28 15:31:08 +0200

    Comit de aplicacion mateo revisado

---
+++ public/script/scriptFeed.js
@@ -0,0 +1,304 @@
+document.addEventListener('DOMContentLoaded', function() {
+    cargarFeed();
+});
+
+function cargarFeed() {
+    $.ajax({
+        type: 'GET',
+        url: '/cargarFeed',
+        success: function(response) {
+            if (response && response.publicaciones && response.publicaciones.length > 0) {
+                var feed = $('#feed'); // Selecciona el contenedor con jQuery
+                feed.empty(); // Limpia el contenido existente
+
+                $.each(response.publicaciones, function(index, publicacion) {
+                    const imgContainer = $('<div>', {
+                        'class': 'image-container',
+                        'style': 'position: relative; margin: 10px auto;'
+                    });
+
+                    var imgElement = $('<img>', {
+                        src: `/${publicacion.imagePath}`,
+                        'class': 'feed-img',
+                        'click': function() {
+                            $('#modalImage').attr('src', `/${publicacion.imagePath}`);
+                            $('#imageModal').modal('show');
+                        }
+                    });
+
+                    var userLabel = $('<div>', {
+                        'class': 'user-label'
+                    });
+
+                    var userLink = $('<a>', {
+                        'href': `/perfil/${publicacion.username}`,
+                        'text': `@${publicacion.username}`,
+                        'style': 'color: blue; text-decoration: underline; cursor: pointer;'
+                    });
+
+                    userLabel.append(userLink);
+
+                    var descripcionDiv = $('<div>', {
+                        'class': 'descripcion'
+                    });
+
+                    if (publicacion.descripcion) {
+                        var shortText = `<strong>${publicacion.username}</strong> ${publicacion.descripcion.substring(0, 100)}... `;
+                        var fullText = `<strong>${publicacion.username}</strong> ${publicacion.descripcion}`;
+                        var isShort = true;
+                    
+                        if (publicacion.descripcion.length > 100) {
+                            descripcionDiv.html(shortText);
+                    
+                            var verMasButton = $('<button>', {
+                                'text': 'Ver más',
+                                'style': 'background: none; border: none; color: blue; text-decoration: underline; cursor: pointer;',
+                                'click': function(e) {
+                                    e.preventDefault();
+                                    toggleDescription(descripcionDiv, verMasButton, shortText, fullText, isShort).then(updatedState => {
+                                        isShort = updatedState;
+                                    });
+                                }
+                            });
+                    
+                            descripcionDiv.append(verMasButton);
+                        } else {
+                            descripcionDiv.html(fullText);
+                        }
+                    } else {
+                        descripcionDiv.html(`<strong>${publicacion.username}</strong>`);
+                    }
+                    
+
+                    var likesLabel = $('<div>', {
+                        'class': 'likes-label',
+                        'text': `${publicacion.meGustas} Me gusta`
+                    });
+
+                    // Uso de la función showLike para obtener el HTML del botón de me gusta
+                    showLike(publicacion._id).then(likeButtonHtml => {
+                        var likeButton = $('<button>', {
+                            'class': 'like-button',
+                            'html': likeButtonHtml,
+                            'click': function() {
+                                addLike(publicacion._id);
+                            }
+                        });
+
+                        var commentButton = $('<button>', {
+                            'class': 'comment-button',
+                            'text': 'Comentar',
+                            'click': function() {
+                                var comentarioTexto = prompt('Introduce tu comentario:');
+                                if (comentarioTexto) {
+                                    addComment(publicacion._id, comentarioTexto);
+                                }
+                            }
+                        });
+
+                        var followButton = $('<button>', {
+                            'class': 'follow-button',
+                            'text': 'Seguir',
+                            'click': function() {
+                                seguirUsuario(publicacion.username);
+                            }
+                        });
+
+                        var unfollowButton = $('<button>', {
+                            'class': 'unfollow-button',
+                            'text': 'Dejar de Seguir',
+                            'click': function() {
+                                dejarDeSeguirUsuario(publicacion.username);
+                            }
+                        });
+
+                        var deleteButton = $('<button>', {
+                            'class': 'delete-button',
+                            'text': 'Eliminar',
+                            'click': function() {
+                                if (confirm('¿Estás seguro de que quieres eliminar esta foto?')) {
+                                    deletePhoto(publicacion._id);
+                                }
+                            }
+                        });
+
+                        var commentsContainer = $('<div>', {
+                            'class': 'comments-container'
+                        });
+
+                        $.each(publicacion.comentarios, function(index, comentario) {
+                            var commentElement = $('<div>', {
+                                'class': 'comment',
+                                'text': `@${comentario.usuario}: ${comentario.texto}`
+                            });
+                            commentsContainer.append(commentElement);
+                        });
+
+                        imgContainer.append(imgElement)
+                            .append(userLabel)
+                            .append(descripcionDiv)
+                            .append(likesLabel)
+                            .append(likeButton)
+                            .append(commentButton)
+                            .append(followButton)
+                            .append(unfollowButton)
+                            .append(deleteButton)
+                            .append(commentsContainer);
+
+                        feed.append(imgContainer);
+                    }).catch(error => {
+                        console.error('Error al obtener el botón de me gusta:', error);
+                    });
+                });
+            } else {
+                console.log('No hay publicaciones en el feed.');
+            }
+        },
+        error: function(error) {
+            console.error('Error al cargar el feed:', error);
+        }
+    });
+}
+function subirImagen() {
+    const fileInput = document.getElementById('inputImagen');
+    const descripcion = document.getElementById('inputDescripcion');
+    //AÑADO CATEGORIA
+    const categoriaInput = document.getElementById('categoria');
+    const formData = new FormData();
+    formData.append('imagen', fileInput.files[0]);
+    formData.append('descripcion', descripcion.value); 
+    formData.append('categoria', categoriaInput.value);
+
+
+    $.ajax({
+        type: 'POST',
+        url: '/upload',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            console.log('Imagen subida correctamente');
+            console.log('Ruta de la imagen:', response.imagePath);
+            alert('Imagen subida correctamente');
+
+            // Cerrar el modal después de la subida exitosa
+            $('#uploadModal').modal('hide');
+            cargarPublicacionesUsuario();
+            cargarPerfil(); // Actualizar datos del perfil después de subir la imagen
+        },
+        error: function (error) {
+            console.error('Error al subir la imagen:', error);
+        }
+    });
+}
+
+function toggleDescription(descripcionDiv, verMasButton, shortText, fullText, isShort) {
+    return new Promise((resolve) => {
+        if (isShort) {
+            descripcionDiv.html(fullText).append(verMasButton.text('Ver menos'));
+            resolve(false);
+        } else {
+            descripcionDiv.html(shortText).append(verMasButton.text('Ver más'));
+            resolve(true);
+        }
+    });
+}
+function addLike(publicacionId) {
+    $.ajax({
+        type: 'POST',
+        url: '/me-gusta',
+        data: JSON.stringify({ publicacionId: publicacionId }),
+        contentType: 'application/json',
+        success: function(response) {
+            if (response.status) {
+                console.log(response.status);
+            } else {
+                console.log(response.status);
+            }
+            cargarFeed(); // Recargar el feed para actualizar el número de "me gusta"
+        },
+        error: function(error) {
+            console.error('Error al añadir "me gusta":', error);
+        }
+    });
+}
+
+function showLike(publicacionId) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: 'POST',
+            url: '/me-gusta-o-no',
+            data: JSON.stringify({ publicacionId: publicacionId }),
+            contentType: 'application/json',
+            success: function(response) {
+                var likebutton;
+                if (response.status) {
+                    likebutton = '<img src="/images/me-gusta.png" alt="Me gusta">';
+                } else {
+                    likebutton = '<img src="/images/me-gusta2.png" alt="Me gusta">';
+                }
+                resolve(likebutton);
+            },
+            error: function(error) {
+                console.error('Error al verificar "me gusta":', error);
+                reject(error);
+            }
+        });
+    });
+}
+
+function addComment(publicacionId, texto) {
+    $.ajax({
+        type: 'POST',
+        url: '/comentario',
+        data: JSON.stringify({ publicacionId: publicacionId, texto: texto }),
+        contentType: 'application/json',
+        success: function(response) {
+            alert(response.message);
+            cargarFeed(); // Recargar el feed para mostrar el nuevo comentario
+        },
+        error: function(error) {
+            console.error('Error al añadir comentario:', error);
+        }
+    });
+}
+
+function seguirUsuario(username) {
+    $.ajax({
+        type: 'POST',
+        url: '/seguir',
+        data: JSON.stringify({ username: username }),
+        contentType: 'application/json',
+        success: function(response) {
+            alert(response.message);
+            cargarFeed(); // Recargar el feed para actualizar el estado de seguimiento
+        },
+        error: function(error) {
+            if (error.responseJSON && error.responseJSON.message) {
+                alert(error.responseJSON.message);
+            } else {
+                console.error('Error al seguir al usuario:', error);
+            }
+        }
+    });
+}
+
+function dejarDeSeguirUsuario(username) {
+    $.ajax({
+        type: 'POST',
+        url: '/dejar-de-seguir',
+        data: JSON.stringify({ username: username }),
+        contentType: 'application/json',
+        success: function(response) {
+            alert(response.message);
+            cargarFeed(); // Recargar el feed para actualizar el estado de seguimiento
+        },
+        error: function(error) {
+            if (error.responseJSON && error.responseJSON.message) {
+                alert(error.responseJSON.message);
+            } else {
+                console.error('Error al dejar de seguir al usuario:', error);
+            }
+        }
+    });
+}
---
+++ public/script/scriptPerfil.js
@@ -0,0 +1,306 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (typeof currentUser !== 'undefined') {
+        const nick_usuario = document.getElementById('nick_usuario');
+        nick_usuario.innerHTML = `${currentUser}`;
+        cargarPerfil(); // Cargar datos del perfil del usuario
+        cargarPublicacionesUsuario(); // Cargar publicaciones del usuario
+    }
+
+    if (typeof profileUser !== 'undefined') {
+        cargarPerfilUsuario(profileUser); // Cargar datos del perfil de otro usuario
+        cargarPublicacionesDeUsuario(profileUser); // Cargar publicaciones de otro usuario
+    }
+});
+
+function cargarPerfil() {
+    $.ajax({
+        type: 'GET',
+        url: '/perfil',
+        success: function (response) {
+            if (response) {
+                $('#nick_usuario').text(response.username);
+                $('#publicaciones').text(`${response.publicaciones} publicaciones`);
+                $('#seguidores').text(`${response.seguidores} seguidores`);
+                $('#seguidos').text(`${response.seguidos} seguidos`);
+                if (response.imagenPerfil) {
+                    $('#fotoperfil').attr('src', `/${response.imagenPerfil}`);
+                } else {
+                    $('#fotoperfil').attr('src', 'images/default-profile.png'); // Imagen de perfil predeterminada
+                }
+            } else {
+                console.error('Error al cargar el perfil del usuario.');
+            }
+        },
+        error: function (error) {
+            console.error('Error al cargar el perfil del usuario:', error);
+        }
+    });
+}
+
+function cargarPerfilUsuario(username) {
+    $.ajax({
+        type: 'GET',
+        url: `/perfil-data/${username}`,
+        success: function (response) {
+            if (response) {
+                $('#nick_usuario').text(response.username);
+                $('#publicaciones').text(`${response.publicaciones} publicaciones`);
+                $('#seguidores').text(`${response.seguidores} seguidores`);
+                $('#seguidos').text(`${response.seguidos} seguidos`);
+                if (response.imagenPerfil) {
+                    $('#fotoperfil').attr('src', `/${response.imagenPerfil}`);
+                } else {
+                    $('#fotoperfil').attr('src', '/images/default-profile.png'); // Imagen de perfil predeterminada
+                }
+            } else {
+                console.error('Error al cargar el perfil del usuario.');
+            }
+        },
+        error: function (error) {
+            console.error('Error al cargar el perfil del usuario:', error);
+        }
+    });
+}
+
+function cargarPublicacionesUsuario() {
+    $.ajax({
+        type: 'GET',
+        url: '/publicaciones-usuario',
+        success: function (response) {
+            if (response && response.publicaciones && response.publicaciones.length > 0) {
+                var galeria = $('#galeria'); // Selecciona el contenedor con jQuery
+                galeria.empty(); // Limpia el contenido existente
+
+                $.each(response.publicaciones, function(index, publicacion) {
+                    const imgContainer = $('<div>', {
+                        'class': 'col-md-4 col-sm-6 image-container'
+                    });
+
+                    var imgElement = $('<img>', {
+                        src: `/${publicacion.imagePath}`,
+                        'class': 'galeria-img',
+                        'click': function() {
+                            $('#modalImage').attr('src', `/${publicacion.imagePath}`);
+                            $('#imageModal').modal('show');
+                        }
+                    });
+
+                    var likesLabel = $('<div>', {
+                        'class': 'likes-label',
+                        'text': `${publicacion.meGustas} Me gusta`
+                    });
+
+                    var likeButton = $('<button>', {
+                        'class': 'like-button',
+                        'html': '<img src="/images/me-gusta.png" alt="Me gusta">'
+                    }).on('click', function() {
+                        addLike(publicacion._id);
+                    });
+
+                    var commentButton = $('<button>', {
+                        'class': 'comment-button',
+                        'html': '<img src="/images/comente.png" alt="Comentar">'
+                    }).on('click', function() {
+                        var comentarioTexto = prompt('Introduce tu comentario:');
+                        if (comentarioTexto) {
+                            addComment(publicacion._id, comentarioTexto);
+                        }
+                    });
+
+                    var commentsContainer = $('<div>', {
+                        'class': 'comments-container'
+                    });
+                    //EL FOR EACH Y LO DE COMENTELEMENT HE PUESTO EL TUYO
+                    $.each(publicacion.comentarios, function(index, comentario) {
+                        var commentElement = $('<div>', {
+                            'class': 'comment'
+                        });
+
+                        var userElement = $('<span>', {
+                            'class': 'comment-user',
+                            'html': `<strong>@${comentario.usuario}</strong>` // Usuario en negrita
+                        });
+
+                        var textElement = $('<span>', {
+                            'class': 'comment-text',
+                            'text': `: ${comentario.texto}`
+                        });
+
+                        commentElement.append(userElement).append(textElement);
+                        commentsContainer.append(commentElement);
+                    });
+
+                    imgContainer.append(imgElement)
+                        .append(likesLabel)
+                        .append(likeButton)
+                        .append(commentButton)
+                        .append(commentsContainer);
+
+                    galeria.append(imgContainer);
+                });
+
+            } else {
+                console.log('El usuario no tiene imágenes.');
+            }
+        },
+        error: function (error) {
+            console.error('Error al cargar las imágenes del usuario:', error);
+        }
+    });
+}
+function subirImagen() {
+    const fileInput = document.getElementById('inputImagen');
+    const descripcion = document.getElementById('inputDescripcion');
+    //AÑADO CATEGORIA
+    const categoriaInput = document.getElementById('categoria');
+    const formData = new FormData();
+    formData.append('imagen', fileInput.files[0]);
+    formData.append('descripcion', descripcion.value); 
+    formData.append('categoria', categoriaInput.value);
+
+
+    $.ajax({
+        type: 'POST',
+        url: '/upload',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            console.log('Imagen subida correctamente');
+            console.log('Ruta de la imagen:', response.imagePath);
+            alert('Imagen subida correctamente');
+
+            // Cerrar el modal después de la subida exitosa
+            $('#uploadModal').modal('hide');
+            cargarPublicacionesUsuario();
+            cargarPerfil(); // Actualizar datos del perfil después de subir la imagen
+        },
+        error: function (error) {
+            console.error('Error al subir la imagen:', error);
+        }
+    });
+}
+
+
+function eliminarFoto(photoId) {
+    $.ajax({
+        type: "DELETE",
+        url: `/delete-photo/${photoId}`,
+        success: function(response) {
+            console.log(response.message);
+            cargarPublicacionesUsuario();
+            cargarPerfil(); // Actualizar datos del perfil después de eliminar la imagen
+        },
+        error: function(error) {
+            console.error('Error al eliminar la foto:', error);
+        }
+    });
+}
+
+function addLike(publicacionId) {
+    $.ajax({
+        type: 'POST',
+        url: '/me-gusta',
+        data: JSON.stringify({ publicacionId: publicacionId }),
+        contentType: 'application/json',
+        success: function(response) {
+            alert(response.message);
+            cargarPublicacionesUsuario(); // Recargar las publicaciones para actualizar el número de "me gusta"
+        },
+        error: function(error) {
+            console.error('Error al añadir "me gusta":', error);
+        }
+    });
+}
+
+function addComment(publicacionId, texto) {
+    $.ajax({
+        type: 'POST',
+        url: '/comentario',
+        data: JSON.stringify({ publicacionId: publicacionId, texto: texto }),
+        contentType: 'application/json',
+        success: function(response) {
+            alert(response.message);
+            cargarPublicacionesUsuario(); // Recargar las publicaciones para mostrar el nuevo comentario
+        },
+        error: function(error) {
+            console.error('Error al añadir comentario:', error);
+        }
+    });
+}
+
+/*function cargarPublicacionesDeUsuario(username) {
+    $.ajax({
+        type: 'GET',
+        url: `/publicaciones-de-usuario/${username}`,
+        success: function (response) {
+            if (response && response.publicaciones && response.publicaciones.length > 0) {
+                var galeria = $('#galeria'); // Selecciona el contenedor con jQuery
+                galeria.empty(); // Limpia el contenido existente
+
+                $.each(response.publicaciones, function(index, publicacion) {
+                    const imgContainer = $('<div>', {
+                        'class': 'col-md-4 col-sm-6 image-container'
+                    });
+
+                    var imgElement = $('<img>', {
+                        src: `/${publicacion.imagePath}`,
+                        'class': 'galeria-img',
+                        'click': function() {
+                            $('#modalImage').attr('src', `/${publicacion.imagePath}`);
+                            $('#imageModal').modal('show');
+                        }
+                    });
+
+                    var likesLabel = $('<div>', {
+                        'class': 'likes-label',
+                        'text': `${publicacion.meGustas} Me gusta`
+                    });
+
+                    var likeButton = $('<button>', {
+                        'class': 'like-button',
+                        'html': '<img src="/images/me-gusta.png" alt="Me gusta">'
+                    }).on('click', function() {
+                        addLike(publicacion._id);
+                    });
+
+                    var commentButton = $('<button>', {
+                        'class': 'comment-button',
+                        'html': '<img src="/images/comente.png" alt="Comentar">'
+                    }).on('click', function() {
+                        var comentarioTexto = prompt('Introduce tu comentario:');
+                        if (comentarioTexto) {
+                            addComment(publicacion._id, comentarioTexto);
+                        }
+                    });
+
+                    var commentsContainer = $('<div>', {
+                        'class': 'comments-container'
+                    });
+
+                    $.each(publicacion.comentarios, function(index, comentario) {
+                        var commentElement = $('<div>', {
+                            'class': 'comment',
+                            'text': `@${comentario.usuario}: ${comentario.texto}`
+                        });
+                        commentsContainer.append(commentElement);
+                    });
+
+                    imgContainer.append(imgElement)
+                        .append(likesLabel)
+                        .append(likeButton)
+                        .append(commentButton)
+                        .append(commentsContainer);
+
+                    galeria.append(imgContainer);
+                });
+
+            } else {
+                console.log('El usuario no tiene imágenes.');
+            }
+        },
+        error: function (error) {
+            console.error('Error al cargar las imágenes del usuario:', error);
+        }
+    });
+}*/
